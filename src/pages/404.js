3e7bc7946c1d49e1b89328291023e61300f30568
omid <techwithomid@gmail.com>
2021-08-12 00:00:35 +0430

    change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,52 +3,38 @@ import { Link } from "gatsby"
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+    color: "#232129",
+    padding: "96px",
+    direction: 'rtl',
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
 }
 
 const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
+    marginBottom: 48,
 }
 
 // markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    return (
+        <main style={pageStyles}>
+            <title>Not found</title>
+            <h1 style={headingStyles}>پیدا نشد</h1>
+            <p style={paragraphStyles}>
+                شرمنده{" "}
+                <span role="img" aria-label="Pensive emoji">
+                    😔
+                </span>{" "}
+                جایی که داری دنبالش می‌گردی رو پیدا نکردم!
+                <br />
+                <Link to="/">صفحه اصلی</Link>.
+            </p>
+        </main>
+    )
 }
 
 export default NotFoundPage
