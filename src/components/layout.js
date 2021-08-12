32ed918c78b206dfe8170b387cca9fd8e8703084
omid <techwithomid@gmail.com>
2021-08-12 17:39:38 +0430

    change ui

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,22 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from '../style/style.module.css'
-
+import { container, navLinkItem, navLinkText, navLinks } from '../style/style.module.css'
 
 const Layout = ({ pageTitle, children }) => {
     return (
-        <div className={container}>
+        <div>
             <title>{pageTitle}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">خانه</Link></li>
-                    <li><Link to="/about">درباره من</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>خانه</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>درباره من</Link>
+                    </li>
                 </ul>
             </nav>
-            <main>
+            <main className={container}>
                 <h1>{pageTitle}</h1>
                 {children}
             </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout.js'
 
-// const pageStyles = {
-//     color: "#232129",
-//     padding: "96px",
-//     direction: 'rtl',
-//     fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// }
-
 const IndexPage = () => {
     return (
         <main>
             <Layout pageTitle="خانه">
                 <p> به وبلاگ من خوش آمدید در اینجا مطالب مرتبط با تکنولوژی و برنامه نویسی به اشتراک میزارم.</p>
+                <p>به زودی مطالب جالبی رو به اشتراک میزارم فعلا مشغول توسعه‌ی وبسایت هستم پس گوش به زنگ باشید :)</p>
             </Layout>
         </main>
     )
