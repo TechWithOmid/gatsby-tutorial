ec7d6208a484d135bb9f8493f061849243fe5555
omid <techwithomid@gmail.com>
2021-08-15 19:18:29 +0430

    add custome img

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
             <Layout pageTitle="خانه">
                 <StaticImage
                     alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-                    src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+                    src="../images/supra.png"
                 />
                 <p> به وبلاگ من خوش آمدید در اینجا مطالب مرتبط با تکنولوژی و برنامه نویسی به اشتراک میزارم.</p>
                 <p>به زودی مطالب جالبی رو به اشتراک میزارم فعلا مشغول توسعه‌ی وبسایت هستم پس گوش به زنگ باشید :)</p>
